3c67f5a6574afb357fd9ba7ef911db04a2830b3e
Nebuska <furkan.cebar@gmail.com>
2022-07-26 05:41:00 +0300

    Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,11 +1,30 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char[][] gameSlot = {{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X','O','X'}};
-        for (int line = 0; line < 3; line++) {
-            for (int column = 0; column < 3; column++) {
-                System.out.print(gameSlot[line][column]);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("> ");
+        String gameSlot = scanner.next();
+
+        if (gameSlot.length() != 9 || !gameSlot.replaceAll("X|O|_","").equals("")) {
+            System.out.println(gameSlot + "isn't in right format!");
+            return;
+        }
+
+        for (int line = -1; line < 4; line++) {
+            if (line == -1 || line == 3) {
+                System.out.println("---------");
+                continue;
+            }
+            for (int column = -1; column < 4; column++) {
+                if (column == -1 || column == 3) {
+                    System.out.print("| ");
+                    continue;
+                }
+                System.out.printf("%s%s",gameSlot.charAt(line*3+column), ' ');
             }
             System.out.println();
         }
